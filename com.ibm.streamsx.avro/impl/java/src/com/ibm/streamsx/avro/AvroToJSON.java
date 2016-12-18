27015f59441602c4a39aa268a4ecfa77617347ed
Frank Ketelaars <fketelaars@nl.ibm.com>
2016-12-18 09:06:58 +0100

    Parameters to specify input and output fields, remove redundant methods

--- com.ibm.streamsx.avro/impl/java/src/com/ibm/streamsx/avro/AvroToJSON.java
+++ com.ibm.streamsx.avro/impl/java/src/com/ibm/streamsx/avro/AvroToJSON.java
@@ -21,7 +21,7 @@ import org.apache.log4j.Logger;
 import com.ibm.streams.operator.AbstractOperator;
 import com.ibm.streams.operator.OperatorContext;
 import com.ibm.streams.operator.OutputTuple;
-import com.ibm.streams.operator.StreamingData.Punctuation;
+import com.ibm.streams.operator.StreamSchema;
 import com.ibm.streams.operator.StreamingInput;
 import com.ibm.streams.operator.StreamingOutput;
 import com.ibm.streams.operator.Tuple;
@@ -55,10 +55,23 @@ public class AvroToJSON extends AbstractOperator {
 
 	private static Logger LOGGER = Logger.getLogger(AvroToJSON.class);
 
-	protected OperatorContext operatorContext;
+	private String inputAvroAttribute = null;
+	private final String DEFAULT_INPUT_AVRO_ATTRIBUTE = "avroBlob";
+	private String outputJsonAttribute = null;
+	private final String DEFAULT_OUTPUT_JSON_ATTRIBUTE = "jsonString";
 
 	protected String avroSchemaFile = "";
 	Schema schema;
+
+	@Parameter(optional = true, description = "The input stream attribute which contains the output Avro blob. This attribute must be of type blob. Default is the sole output attribute when the schema has one attribute otherwise `avroBlob`.")
+	public void setInputAvroAttribute(String inputAvroAttribute) {
+		this.inputAvroAttribute = inputAvroAttribute;
+	}
+
+	@Parameter(optional = true, description = "The output stream attribute which contains the output JSON string. This attribute must be of `rstring` or `ustring` type. Default is the sole input attribute when the schema has one attribute otherwise `jsonString`.")
+	public void setOutputJsonAttribute(String outputJsonAttribute) {
+		this.outputJsonAttribute = outputJsonAttribute;
+	}
 
 	@Parameter(name = "avroSchemaFile", optional = false, description = "File that contains the Avro schema to deserialize the binary message.")
 	public void setAvroSchemaFile(String avroSchemaFile) {
@@ -81,9 +94,31 @@ public class AvroToJSON extends AbstractOperator {
 	public synchronized void initialize(OperatorContext operatorContext) throws Exception {
 		// Must call super.initialize(context) to correctly setup an operator.
 		super.initialize(operatorContext);
-		this.operatorContext = operatorContext;
 		LOGGER.log(TraceLevel.TRACE, "Operator " + operatorContext.getName() + " initializing in PE: "
 				+ operatorContext.getPE().getPEId() + " in Job: " + operatorContext.getPE().getJobId());
+
+		StreamSchema ssOp0 = getOutput(0).getStreamSchema();
+		StreamSchema ssIp0 = getInput(0).getStreamSchema();
+
+		// If no input Avro blob attribute specified, use default
+		if (inputAvroAttribute == null) {
+			if (ssIp0.getAttributeCount() == 1) {
+				inputAvroAttribute = ssIp0.getAttribute(0).getName();
+			} else {
+				inputAvroAttribute = DEFAULT_INPUT_AVRO_ATTRIBUTE;
+			}
+		}
+		LOGGER.log(TraceLevel.TRACE, "Input Avro attribute: " + inputAvroAttribute);
+
+		// If no output JSON attribute specified, use default
+		if (outputJsonAttribute == null) {
+			if (ssOp0.getAttributeCount() == 1) {
+				outputJsonAttribute = ssOp0.getAttribute(0).getName();
+			} else {
+				outputJsonAttribute = DEFAULT_OUTPUT_JSON_ATTRIBUTE;
+			}
+		}
+		LOGGER.log(TraceLevel.TRACE, "Output JSON attribute: " + outputJsonAttribute);
 
 		// Get the Avro schema file to parse the Avro messages
 		LOGGER.log(TraceLevel.TRACE, "Retrieving and parsing Avro schema file " + getAvroSchemaFile());
@@ -93,23 +128,6 @@ public class AvroToJSON extends AbstractOperator {
 
 		LOGGER.log(TraceLevel.TRACE, "AvroToJSON operator initialized, ready to receive tuples");
 
-	}
-
-	/**
-	 * Notification that initialization is complete and all input and output
-	 * ports are connected and ready to receive and submit tuples.
-	 * 
-	 * @throws Exception
-	 *             Operator failure, will cause the enclosing PE to terminate.
-	 */
-	@Override
-	public synchronized void allPortsReady() throws Exception {
-		// This method is commonly used by source operators.
-		// Operators that process incoming tuples generally do not need this
-		// notification.
-		OperatorContext context = getOperatorContext();
-		LOGGER.log(TraceLevel.TRACE, "Operator " + context.getName() + " all ports are ready in PE: "
-				+ context.getPE().getPEId() + " in Job: " + context.getPE().getJobId());
 	}
 
 	/**
@@ -124,49 +142,21 @@ public class AvroToJSON extends AbstractOperator {
 		OutputTuple outTuple = outStream.newTuple();
 
 		// Get the incoming binary Avro record(s)
-		Blob avroMessage = tuple.getBlob(0);
-		LOGGER.log(TraceLevel.TRACE, "Processing Avro message with length " + avroMessage.getLength());
+		Blob avroMessage = tuple.getBlob(inputAvroAttribute);
+		if (LOGGER.isTraceEnabled())
+			LOGGER.log(TraceLevel.TRACE, "Processing Avro message with length " + avroMessage.getLength());
 
 		// Convert the BLOB to GenericRecord object
 		GenericDatumReader<GenericRecord> consumer = new GenericDatumReader<GenericRecord>(schema);
 		ByteArrayInputStream consumedByteArray = new ByteArrayInputStream(avroMessage.getData());
 		Decoder consumedDecoder = DecoderFactory.get().binaryDecoder(consumedByteArray, null);
 		GenericRecord consumedDatum = consumer.read(null, consumedDecoder);
-		LOGGER.log(TraceLevel.TRACE, "JSON representation of Avro message: " + consumedDatum.toString());
+		if (LOGGER.isTraceEnabled())
+			LOGGER.log(TraceLevel.TRACE, "JSON representation of Avro message: " + consumedDatum.toString());
 
 		// Submit new tuple to output port 0
-		outTuple.setString(0, consumedDatum.toString());
+		outTuple.setString(outputJsonAttribute, consumedDatum.toString());
 		outStream.submit(outTuple);
-	}
-
-	/**
-	 * Process an incoming punctuation that arrived on the specified port.
-	 * 
-	 * @param stream
-	 *            Port the punctuation is arriving on.
-	 * @param mark
-	 *            The punctuation mark
-	 * @throws Exception
-	 *             Operator failure, will cause the enclosing PE to terminate.
-	 */
-	@Override
-	public void processPunctuation(StreamingInput<Tuple> stream, Punctuation mark) throws Exception {
-		// For window markers, punctuate all output ports
-		super.processPunctuation(stream, mark);
-	}
-
-	/**
-	 * Shutdown this operator.
-	 * 
-	 * @throws Exception
-	 *             Operator failure, will cause the enclosing PE to terminate.
-	 */
-	public synchronized void shutdown() throws Exception {
-		OperatorContext context = getOperatorContext();
-		LOGGER.log(TraceLevel.TRACE, "Operator " + context.getName() + " shutting down in PE: "
-				+ context.getPE().getPEId() + " in Job: " + context.getPE().getJobId());
-		// Must call super.shutdown()
-		super.shutdown();
 	}
 
 	static final String DESC = "This operator binary Avro messages into a JSON string."
--- com.ibm.streamsx.avro/impl/java/src/com/ibm/streamsx/avro/JSONToAvro.java
+++ com.ibm.streamsx.avro/impl/java/src/com/ibm/streamsx/avro/JSONToAvro.java
@@ -12,6 +12,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericDatumReader;
@@ -26,7 +27,7 @@ import org.apache.log4j.Logger;
 import com.ibm.streams.operator.AbstractOperator;
 import com.ibm.streams.operator.OperatorContext;
 import com.ibm.streams.operator.OutputTuple;
-import com.ibm.streams.operator.StreamingData.Punctuation;
+import com.ibm.streams.operator.StreamSchema;
 import com.ibm.streams.operator.StreamingInput;
 import com.ibm.streams.operator.StreamingOutput;
 import com.ibm.streams.operator.Tuple;
@@ -45,7 +46,7 @@ import com.ibm.streams.operator.model.PrimitiveOperator;
 import com.ibm.streams.operator.types.ValueFactory;
 
 /**
- * Processes Avro tuples and converts them to a JSON string 
+ * Processes Avro tuples and converts them to a JSON string
  * 
  */
 
@@ -60,18 +61,27 @@ public class JSONToAvro extends AbstractOperator {
 
 	private static Logger LOGGER = Logger.getLogger(JSONToAvro.class);
 
-	protected OperatorContext operatorContext;
+	private String inputJsonAttribute = null;
+	private final String DEFAULT_INPUT_JSON_ATTRIBUTE = "jsonString";
+	private String outputAvroAttribute = null;
+	private final String DEFAULT_OUTPUT_AVRO_ATTRIBUTE = "avroBlob";
 
-	protected String avroSchemaFile = "";
-	Schema schema;
+	private String avroSchemaFile = null;
+	private Schema schema;
 
-	@Parameter(name = "avroSchemaFile", optional = false, description = "File that contains the Avro schema to serialize the binary message.")
-	public void setAvroSchemaFile(String avroSchemaFile) {
-		this.avroSchemaFile = avroSchemaFile;
+	@Parameter(optional = true, description = "The input stream attribute which contains the input JSON string. This attribute must be of `rstring` or `ustring` type. Default is the sole input attribute when the schema has one attribute otherwise `jsonString`.")
+	public void setInputJsonAttribute(String inputJsonAttribute) {
+		this.inputJsonAttribute = inputJsonAttribute;
 	}
 
-	public String getAvroSchemaFile() {
-		return avroSchemaFile;
+	@Parameter(optional = true, description = "The ouput stream attribute which contains the output Avro blob. This attribute must be of type blob. Default is the sole output attribute when the schema has one attribute otherwise `avroBlob`.")
+	public void setOutputAvroAttribute(String outputAvroAttribute) {
+		this.outputAvroAttribute = outputAvroAttribute;
+	}
+
+	@Parameter(optional = false, description = "File that contains the Avro schema to serialize the binary message.")
+	public void setAvroSchemaFile(String avroSchemaFile) {
+		this.avroSchemaFile = avroSchemaFile;
 	}
 
 	/**
@@ -86,35 +96,40 @@ public class JSONToAvro extends AbstractOperator {
 	public synchronized void initialize(OperatorContext operatorContext) throws Exception {
 		// Must call super.initialize(context) to correctly setup an operator.
 		super.initialize(operatorContext);
-		this.operatorContext = operatorContext;
 		LOGGER.log(TraceLevel.TRACE, "Operator " + operatorContext.getName() + " initializing in PE: "
 				+ operatorContext.getPE().getPEId() + " in Job: " + operatorContext.getPE().getJobId());
 
+		StreamSchema ssOp0 = getOutput(0).getStreamSchema();
+		StreamSchema ssIp0 = getInput(0).getStreamSchema();
+
+		// If no input JSON attribute specified, use default
+		if (inputJsonAttribute == null) {
+			if (ssIp0.getAttributeCount() == 1) {
+				inputJsonAttribute = ssIp0.getAttribute(0).getName();
+			} else {
+				inputJsonAttribute = DEFAULT_INPUT_JSON_ATTRIBUTE;
+			}
+		}
+		LOGGER.log(TraceLevel.TRACE, "Input JSON attribute: " + inputJsonAttribute);
+
+		// If no output Avro blob attribute specified, use default
+		if (outputAvroAttribute == null) {
+			if (ssOp0.getAttributeCount() == 1) {
+				outputAvroAttribute = ssOp0.getAttribute(0).getName();
+			} else {
+				outputAvroAttribute = DEFAULT_OUTPUT_AVRO_ATTRIBUTE;
+			}
+		}
+		LOGGER.log(TraceLevel.TRACE, "Output Avro attribute: " + outputAvroAttribute);
+
 		// Get the Avro schema file to parse the Avro messages
-		LOGGER.log(TraceLevel.TRACE, "Retrieving and parsing Avro schema file " + getAvroSchemaFile());
-		InputStream avscInput = new FileInputStream(getAvroSchemaFile());
+		LOGGER.log(TraceLevel.TRACE, "Retrieving and parsing Avro schema file " + avroSchemaFile);
+		InputStream avscInput = new FileInputStream(avroSchemaFile);
 		Schema.Parser parser = new Schema.Parser();
 		schema = parser.parse(avscInput);
 
 		LOGGER.log(TraceLevel.TRACE, "JSONToAvro operator initialized, ready to receive tuples");
 
-	}
-
-	/**
-	 * Notification that initialization is complete and all input and output
-	 * ports are connected and ready to receive and submit tuples.
-	 * 
-	 * @throws Exception
-	 *             Operator failure, will cause the enclosing PE to terminate.
-	 */
-	@Override
-	public synchronized void allPortsReady() throws Exception {
-		// This method is commonly used by source operators.
-		// Operators that process incoming tuples generally do not need this
-		// notification.
-		OperatorContext context = getOperatorContext();
-		LOGGER.log(TraceLevel.TRACE, "Operator " + context.getName() + " all ports are ready in PE: "
-				+ context.getPE().getPEId() + " in Job: " + context.getPE().getJobId());
 	}
 
 	/**
@@ -124,16 +139,20 @@ public class JSONToAvro extends AbstractOperator {
 	@Override
 	public final void process(StreamingInput<Tuple> inputStream, Tuple tuple) throws Exception {
 
-		// Create a new tuple for output port 0
+		String jsonInput = tuple.getString(inputJsonAttribute);
+
+		if (LOGGER.isTraceEnabled())
+			LOGGER.log(TraceLevel.TRACE, "Input JSON string: " + jsonInput);
+
+		// Create a new tuple for output port 0 and copy over any matching
+		// attributes
 		StreamingOutput<OutputTuple> outStream = getOutput(0);
 		OutputTuple outTuple = outStream.newTuple();
+		outTuple.assign(tuple);
 
-		// Get the incoming JSON record
-		String jsonString = tuple.getString(0);
-
-		// Convert the String to a GenericRecord object
-		GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
-		ByteArrayInputStream jsonByteArray = new ByteArrayInputStream(jsonString.getBytes());
+		// Convert the JSON string to a GenericRecord object
+		final GenericDatumReader<GenericRecord> datumReader = new GenericDatumReader<GenericRecord>(schema);
+		ByteArrayInputStream jsonByteArray = new ByteArrayInputStream(jsonInput.getBytes(StandardCharsets.UTF_8));
 		ByteArrayOutputStream avroByteArray = new ByteArrayOutputStream();
 		DataInputStream jsonDis = new DataInputStream(jsonByteArray);
 		GenericDatumWriter<GenericRecord> avroWriter = new GenericDatumWriter<GenericRecord>(schema);
@@ -143,41 +162,12 @@ public class JSONToAvro extends AbstractOperator {
 		avroWriter.write(datum, encoder);
 		encoder.flush();
 
-		LOGGER.log(TraceLevel.TRACE, "Length of Avro message: " + avroByteArray.size());
+		if (LOGGER.isTraceEnabled())
+			LOGGER.log(TraceLevel.TRACE, "Length of generated Avro message: " + avroByteArray.size());
 
 		// Submit new tuple to output port 0
-		outTuple.setBlob(0, ValueFactory.newBlob(avroByteArray.toByteArray()));
+		outTuple.setBlob(outputAvroAttribute, ValueFactory.newBlob(avroByteArray.toByteArray()));
 		outStream.submit(outTuple);
-	}
-
-	/**
-	 * Process an incoming punctuation that arrived on the specified port.
-	 * 
-	 * @param stream
-	 *            Port the punctuation is arriving on.
-	 * @param mark
-	 *            The punctuation mark
-	 * @throws Exception
-	 *             Operator failure, will cause the enclosing PE to terminate.
-	 */
-	@Override
-	public void processPunctuation(StreamingInput<Tuple> stream, Punctuation mark) throws Exception {
-		// For window markers, punctuate all output ports
-		super.processPunctuation(stream, mark);
-	}
-
-	/**
-	 * Shutdown this operator.
-	 * 
-	 * @throws Exception
-	 *             Operator failure, will cause the enclosing PE to terminate.
-	 */
-	public synchronized void shutdown() throws Exception {
-		OperatorContext context = getOperatorContext();
-		LOGGER.log(TraceLevel.TRACE, "Operator " + context.getName() + " shutting down in PE: "
-				+ context.getPE().getPEId() + " in Job: " + context.getPE().getJobId());
-		// Must call super.shutdown()
-		super.shutdown();
 	}
 
 	static final String DESC = "This operator binary Avro messages into a JSON string."

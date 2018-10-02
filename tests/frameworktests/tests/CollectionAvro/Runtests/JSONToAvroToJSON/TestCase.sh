95a33269f37e9a70b1d1d0ddd6d731730854e810
joergboe <joergboe@de.ibm.com>
2018-10-02 11:39:19 +0200

    Test JSONToAvro add tests submitOnPunct tuplesPerMessage timePerMessage

--- tests/frameworktests/tests/CollectionAvro/Runtests/JSONToAvroToJSON/TestCase.sh
+++ tests/frameworktests/tests/CollectionAvro/Runtests/JSONToAvroToJSON/TestCase.sh
@@ -1,14 +1,56 @@
-PREPS='copyOnly'
+#--variantList='embedAvroSchema_false submitOnPunct tuplesPerMessage timePerMessage'
+
+if [[ $TTRO_variantCase == embedAvroSchema_false ]]; then
+	setCategory 'quick'
+fi
+
+PREPS='copyAndMorphSpl'
 
 STEPS=(
-	"splCompile"
+	'splCompile'
 	'submitJob'
 	'checkJobNo'
 	'waitForFinAndHealth'
 	'cancelJobAndLog'
-	'echoExecuteInterceptAndSuccess diff data/Tuples data/TuplesExpected'
-	'linewisePatternMatchInterceptAndSuccess data/WindowMarker "" "{seq_=100,typ_=\"w\",jsonMessage=\"\"}"'
-	'linewisePatternMatchInterceptAndSuccess data/FinalMarker ""  "{seq_=101,typ_=\"f\",jsonMessage=\"\"}"'
+	'checkTuples'
+	'checkWindowMarker'
+	'checkFinalMarker'
 )
 
 FINS='cancelJobAndLog'
+
+checkTuples() {
+	case "$TTRO_variantCase" in
+	timePerMessage)
+		local count=$(wc -l data/Tuples | cut -f1 -d' ')
+		if [[ $count -ne 100 ]]; then
+			setFailure "Number of received tuples in ne 100. Count is: $count"
+		fi;;
+	tuplesPerMessage)
+		echoExecuteInterceptAndSuccess diff data/Tuples data/TuplesExpected_tuplesPerMessage;;
+	*)
+		echoExecuteInterceptAndSuccess diff data/Tuples data/TuplesExpected;;
+	esac
+}
+
+checkWindowMarker() {
+	case "$TTRO_variantCase" in
+	timePerMessage)
+		;;
+	tuplesPerMessage)
+		echoExecuteInterceptAndSuccess diff data/WindowMarker data/WindowMarkerExpected_tuplesPerMessage;;
+	*)
+		linewisePatternMatchInterceptAndSuccess data/WindowMarker '' '{seq_=100,typ_="w",jsonMessage=""}';;
+	esac
+}
+
+checkFinalMarker() {
+	case "$TTRO_variantCase" in
+	timePerMessage)
+		;;
+	tuplesPerMessage)
+		linewisePatternMatchInterceptAndSuccess data/FinalMarker ''  '{seq_=110,typ_="f",jsonMessage=""}';;
+	*)
+		linewisePatternMatchInterceptAndSuccess data/FinalMarker ''  '{seq_=101,typ_="f",jsonMessage=""}';;
+	esac
+}

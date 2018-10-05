1deb7925b1b283f6afb1c8a9c0912d4ea09cfbf0
joergboe <joergboe@de.ibm.com>
2018-10-05 10:40:21 +0200

    Test content of avro files with external tool in test case JSONToAvro

--- tests/frameworktests/tests/CollectionAvro/Runtests/JSONToAvro/TestCase.sh
+++ tests/frameworktests/tests/CollectionAvro/Runtests/JSONToAvro/TestCase.sh
@@ -1,3 +1,4 @@
+#--timeout=900
 
 PREPS='copyOnly'
 
@@ -8,16 +9,55 @@ STEPS=(
 	'waitForFinAndHealth'
 	'cancelJobAndLog'
 	'checkTuples'
-	'linewisePatternMatchInterceptAndSuccess data/WindowMarker "true" "{seq_=100,typ_=\"w\",avroMessage=}"'
-	'linewisePatternMatchInterceptAndSuccess data/FinalMarker ""  "{seq_=101,typ_=\"f\",avroMessage=}"'
+	'linewisePatternMatchInterceptAndSuccess data/WindowMarker "true" "{seq_=199,typ_=\"w\",avroMessage=}"'
+	'linewisePatternMatchInterceptAndSuccess data/FinalMarker ""  "{seq_=200,typ_=\"f\",avroMessage=}"'
+	'checkContent'
 )
 
 FINS='cancelJobAndLog'
 
 checkTuples() {
-	local i
+	local i j
 	for ((i=0; i<100; i++)); do
-		linewisePatternMatchInterceptAndSuccess data/Tuples "true" "{seq_=$i,typ_=\"t\",avroMessage*"
+		j=$((i*2))
+		linewisePatternMatchInterceptAndSuccess data/Tuples "true" "{seq_=$j,typ_=\"t\",avroMessage*"
+	done
+	return 0
+}
+
+checkContent() {
+	if ! jq -h &> /dev/zero; then
+		printError "Json tool jq not available"
+		return 0
+	fi
+	local i
+	local reference actval
+	for ((i=0; i<100; i++)); do
+		java -jar "$TTRO_inputDir/../avro-tools-1.8.2.jar" 'tojson' "data/AvroFile_$i" > "data/CompFile_$i"
+		reference="$(jq '.username' "data/JsonFile_$i")"
+		actval="$(jq '.username' "data/CompFile_$i")"
+		if [[ $actval == $reference ]]; then
+			echo "Matching username $i: $actval"
+		else
+			setFailure "Difference in username file data/CompFile_$i"
+			break
+		fi
+		reference="$(jq '.tweet' "data/JsonFile_$i")"
+		actval="$(jq '.tweet' "data/CompFile_$i")"
+		if [[ $actval == $reference ]]; then
+			echo "Matching tweet $i: $actval"
+		else
+			setFailure "Difference in tweet file data/CompFile_$i"
+			break
+		fi
+		reference="$(jq '.tweettime' "data/JsonFile_$i")"
+		actval="$(jq '.tweettime' "data/CompFile_$i")"
+		if [[ $actval == $reference ]]; then
+			echo "Matching tweettime $i: $actval"
+		else
+			setFailure "Difference in tweettime file data/CompFile_$i"
+			break
+		fi
 	done
 	return 0
 }
--- tests/frameworktests/tests/CollectionAvro/TestSuite.sh
+++ tests/frameworktests/tests/CollectionAvro/TestSuite.sh
@@ -1,2 +1,11 @@
 # The common test suite for avro toolkit tests
 import "$TTRO_scriptDir/streamsutils.sh"
+
+PREPS=('checkJq')
+
+checkJq() {
+	if ! jq -h &> /dev/zero; then
+		printError "Json tool jq not available! Install package jq"
+		return 0
+	fi
+}
